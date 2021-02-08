9fba21b72b029c189576c8ddc40d2def1689d697
Vasil Slavchev <vasilslavchev@gmail.com>
2021-02-08 13:19:28 +0200

    task complited refactored minified etc.

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,18 @@
 const gulp = require('gulp');
 const sass = require('gulp-sass');
 const del = require('del');
+const htmlmin = require('gulp-htmlmin');
 
 gulp.task('styles', () => {
     return gulp.src('sass/**/*.scss')
         .pipe(sass({outputStyle: 'expanded'}).on('error', sass.logError)) // compressed / compact / expanded / nested
         .pipe(gulp.dest('./css/'));
+});
+
+gulp.task('minify-html', function() {
+    return gulp.src('index.html')
+        .pipe(htmlmin({ collapseWhitespace: true }))
+        .pipe(gulp.dest('public/'));
 });
 
 gulp.task('clean', () => {
@@ -14,10 +21,11 @@ gulp.task('clean', () => {
     ]);
 });
 
-gulp.task('default', gulp.series(['clean', 'styles']));
+gulp.task('default', gulp.series(['clean', 'styles', 'minify-html']));
 
 gulp.task('watch', () => {
     gulp.watch('sass/**/*.scss', (done) => {
         gulp.series(['clean', 'styles'])(done);
     });
+    gulp.watch('*.html', ['minify-html']);
 });
